4bfac30dce8e8b4397cb7950e24b1c53da0a54fc
oliverldx <176514610@qq.com>
2020-03-18 04:31:18 +0800

    1. update the docker compose

--- mall-security/src/main/java/com/macro/mall/security/component/JwtAuthenticationTokenFilter.java
+++ mall-security/src/main/java/com/macro/mall/security/component/JwtAuthenticationTokenFilter.java
@@ -38,8 +38,7 @@ public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
                                     HttpServletResponse response,
                                     FilterChain chain) throws ServletException, IOException {
         String authHeader = request.getHeader(this.tokenHeader);
-        if (authHeader != null && authHeader.startsWith(this.tokenHead)) {
-            String authToken = authHeader.substring(this.tokenHead.length());// The part after "Bearer "
+        if (authHeader != null && authHeader.startsWith(this.tokenHead)) { String authToken = authHeader.substring(this.tokenHead.length());// The part after "Bearer "
             String username = jwtTokenUtil.getUserNameFromToken(authToken);
             LOGGER.info("checking username:{}", username);
             if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
